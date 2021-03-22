7a794772d3413df688f70e5ae5d4965fb8de8114
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-22 22:46:25 +0100

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,14 @@
+const mili = [10, 25, 4]; // Do not change this line
 
+function myArr(mili) {
+    'use strict';
+    // Only change code below this line
+    // Using mili = [4, 10, 25] would be invalid
+    mili[0] = 4;
+    mili[1] = 10;
+    mili[2] = 25;
+    // Only change code above this line
+    return mili;
+}
+console.log(myArr(mili)); // Change this line
+module.exports = myArr;
