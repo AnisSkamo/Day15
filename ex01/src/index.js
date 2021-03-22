b1c9e9eb32503a838abc3d326658e627764cb530
AnisSkamo <80179678+AnisSkamo@users.noreply.github.com>
2021-03-22 19:48:10 +0100

    Add files via upload

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,13 @@
-
+// Only change code below this line
+let myName = "Padwans";
+let message = "Padwans";
+// Only change code above this line
+myName = "Padwans";
+function myMsg(name) {
+    "use strict";
+    message = "Hello, " + name + "!";
+    
+    return message;
+}
+console.log(myMsg(myName));
+module.exports = myMsg;
